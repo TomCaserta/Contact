75abe65cc676440126c5b482fe866aa7e7948131
Thomas Caserta <tom@caserta.co.uk>
2016-11-21 23:12:52 +0000

    Wrestled with webpack to get it to correctly output ES6 and load Styles and SVGs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractText = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   devtool: 'eval',
@@ -14,13 +16,31 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractText( "styles.css", {
+        allChunks: true
+    })
   ],
+  resolve : {
+    alias: {
+      "flags": path.join(__dirname, "node_modules", "country-flags", "svg")
+    },
+    extensions: [ '', '.js', '.jsx', '.svg' ]
+  },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loaders: [ 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: [ 'babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.scss$/,
+        loader:
+          ExtractText.extract("style", "css!sass")
+      },
+      { test: /\.svg/, loaders: ["file-loader?name=[path][name].[ext]"]},
+      { test: /\.(jpe?g|png|gif)$/i, loader:"file" }
+    ]
   }
 };
