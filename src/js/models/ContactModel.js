2e25c654cda06e1da00d500a2be4a4e0afb78737
Thomas Caserta <tom@caserta.co.uk>
2016-11-24 21:27:57 +0000

    Added a method to check if it has the same details as another contact, a way to retreive the country name and renamed toJSON to toJS

--- src/js/models/ContactModel.js
+++ src/js/models/ContactModel.js
@@ -1,7 +1,19 @@
-import {observable, computed} from 'mobx';
-import {default as md5} from 'md5';
+import { observable, computed } from 'mobx';
+import md5 from 'md5';
+import getCountries from 'country-list';
+
+const COUNTRIES = getCountries();
 
 export default class ContactModel {
+
+  static hasSameDetails(contact, other) {
+    if (other.firstName !== contact.firstName) return false;
+    if (other.lastName !== contact.lastName) return false;
+    if (other.email !== contact.email) return false;
+    if (other.country !== contact.country) return false;
+    return true;
+  }
+
   store;
   id;
   @observable firstName;
@@ -9,43 +21,55 @@ export default class ContactModel {
   @observable country;
   @observable email;
 
-  @computed get fullName () {
-    return this.firstName + " " + this.lastName;
+  @computed get fullName() {
+    return `${this.firstName} ${this.lastName}`;
   }
 
-  constructor (store, id, firstName, lastName, email, country) {
+  @computed get countryName() {
+    return COUNTRIES.getName(this.country);
+  }
+
+  constructor(store, id, firstName, lastName, email, country) {
     this.store = store;
     this.id = id;
+    this.fillFrom({ firstName, lastName, email, country });
+  }
+
+  getProfilePicture(size = 50) {
+    return `https://www.gravatar.com/avatar/${md5(this.email.trim().toLowerCase())}?d=identicon&=${size}`;
+  }
+
+  removeContact() {
+    this.store.removeContact(this);
+  }
+
+  fillFrom({ firstName, lastName, email, country }) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.country = country;
   }
 
-  getProfilePicture (size = 50) {
-    return "https://www.gravatar.com/avatar/"+md5(this.email.trim().toLowerCase())+"?s="+size;
+  hasSameDetails(other) {
+    return ContactModel.hasSameDetails(this, other);
   }
 
-  removeContact () {
-    this.store.removeContact(this);
-  }
-
-
-  // Method probably not needed outright
-  // but we want to make sure in the future
-  // if we add to this that we only have the
-  // fields we want saved.
-  toJson () {
+  toJS() {
     return {
-      "id": this.id,
-      "firstName": this.firstName,
-      "lastName": this.lastName,
-      "country": this.country,
-      "email": this.email
+      id: this.id,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      country: this.country,
+      email: this.email,
     };
   }
 
-  static fromJson (store, contact) {
-    return new ContactModel(store, contact.id, contact.firstName, contact.lastName, contact.email, contact.country);
+  static fromJson(store, contact) {
+    return new ContactModel(store,
+      contact.id,
+      contact.firstName,
+      contact.lastName,
+      contact.email,
+      contact.country);
   }
 }
