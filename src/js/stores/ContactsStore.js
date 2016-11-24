38de1b712a862abd153270db245d84eb0ca11ff8
Thomas Caserta <tom@caserta.co.uk>
2016-11-24 21:28:39 +0000

    Renamed toJSON to toJS to match mobx, fixed a bug with loading contacts not keeping their ID

--- src/js/stores/ContactsStore.js
+++ src/js/stores/ContactsStore.js
@@ -1,74 +1,73 @@
-import {observable, computed, reaction} from 'mobx';
-import {default as escapeStringRegexp} from 'escape-string-regexp';
+import { observable, computed } from 'mobx';
+import escapeStringRegexp from 'escape-string-regexp';
 
-import ContactModel from "../models/ContactModel";
+import ContactModel from '../models/ContactModel';
 
 export default class ContactsStore {
   _currentID = null;
   @observable contacts = [];
-  @observable filter = "";
+  @observable filter = '';
 
   /* TODO: Consider replacing this with a UUID generator */
-  @computed get nextID () {
-    if (this._currentID == null) {
+  @computed get nextID() {
+    if (this._currentID === null) {
       this._currentID = this.computeHighestID();
     }
-    this._currentID++;
+    this._currentID += 1;
     return this._currentID;
   }
 
-  @computed get filteredContacts () {
-    if (this.filter === "") {
+  @computed get filteredContacts() {
+    if (this.filter === '') {
       return this.sortedContacts;
     }
-    let filterRegex = new RegExp(".*"+escapeStringRegexp(this.filter)+".*", "i");
-    return this.sortedContacts.filter((contact) => {
-      return filterRegex.test(contact.fullName);
+    const filterRegex = new RegExp(`.*${escapeStringRegexp(this.filter)}.*`, 'i');
+    return this.sortedContacts.filter(contact => filterRegex.test(contact.fullName));
+  }
+
+  @computed get sortedContacts() {
+    return this.contacts.sort((a, b) => {
+      const nameA = a.fullName.toUpperCase();
+      const nameB = b.fullName.toUpperCase();
+      return nameA.localeCompare(nameB);
     });
   }
 
-  @computed get sortedContacts () {
-    return this.contacts.sort(function(a, b) {
-        var nameA = a.fullName.toUpperCase();
-        var nameB = b.fullName.toUpperCase();
-        return nameA.localeCompare(nameB);
-    });
-  }
-
-  computeHighestID () {
-    var max = 0;
+  computeHighestID() {
+    let max = 0;
     this.contacts.forEach((contact) => {
       if (contact.id > max) max = contact.id;
     });
     return max;
   }
 
-  findById (id) {
-    return this.contacts.find((contact) => contact.id == id);
+  findById(id) {
+    return this.contacts.find(contact => contact.id === id);
   }
 
-  addContact ({ firstName, lastName, email, country }) {
-    let contact = new ContactModel(this, this.nextID, firstName, lastName, email, country);
+  addContact({ id = this.nextID, firstName, lastName, email, country }) {
+    const contact = new ContactModel(this, id, firstName, lastName, email, country);
     this.contacts.push(contact);
     return contact;
   }
 
-  removeContact (contact) {
+  removeContact(contact) {
     this.contacts.remove(contact);
   }
 
-  load (contactsData) {
-    let contacts = [];
+  load(contactsData) {
     for (let i = 0; i < contactsData.length; i++) {
-      let contact = contactsData[i];
+      const contact = contactsData[i];
       if (contact instanceof ContactModel) {
         this.contacts.push(contact);
-      }
-      else {
+      } else {
         this.addContact(contact);
       }
     }
     return this;
   }
 
+  toJS() {
+    return this.contacts.map(e => e.toJS());
+  }
 }
