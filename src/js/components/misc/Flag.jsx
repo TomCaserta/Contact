45b0fedad7a933fb032eb8c422fb763a6833748a
Thomas Caserta <tom@caserta.co.uk>
2016-11-24 21:22:53 +0000

    Flag component that outputs a svg image of a flag, if one is not found just shows the country code, much like unicode

--- src/js/components/misc/Flag.jsx
+++ src/js/components/misc/Flag.jsx
@@ -1,8 +1,15 @@
-import React, {Component} from "react";
+import React, { PropTypes } from 'react';
 
-export default class Flag extends Component {
-
-  render () {
-    return (<img src={require("flags/"+this.props.country.toLowerCase()+".svg")} style={{height: this.props.height}} />);
+export default function Flag(props) {
+  try {
+    return (<img alt={props.country} className={props.className} src={require(`flags/${props.country.toLowerCase()}.svg`)} style={{ height: props.height }} />);
+  } catch (e) {
+    return (<div className={`default-flag ${props.className}`} style={{ height: props.height, lineHeight: `${props.height}px` }}>{props.country.toUpperCase() }</div>);
   }
 }
+
+Flag.propTypes = {
+  country: PropTypes.string,
+  className: PropTypes.string,
+  height: PropTypes.number.isRequired,
+};
