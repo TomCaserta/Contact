14ae1edf2ac1ba7dbc3bffad79a0ecde4a7a4099
Thomas Caserta <tom@caserta.co.uk>
2016-11-24 21:26:34 +0000

    Added toolbar support

--- src/js/components/AppLayout.jsx
+++ src/js/components/AppLayout.jsx
@@ -1,22 +1,30 @@
-import React, {Component} from "react";
-import {inject, observer} from "mobx-react";
-import logoSvg from "../../images/logo.svg";
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import FaPlus from 'react-icons/lib/fa/plus';
+import logoSvg from '../../images/logo.svg';
+import Toolbar from './toolbar/Toolbar';
 
-@inject("routing")
-@observer
-export default class AppLayout extends Component {
-  render () {
-    const { main, sidebar } = this.props;
-    return (<div className="contacts-app">
-      <div className="panel">
-        <img src={logoSvg} className="logo" />
-      </div>
-      <div className="sidebar">
-        {sidebar}
-      </div>
-      <div className="main">
-        {main}
-      </div>
-    </div>);
-  }
+export default function AppLayout(props) {
+  const { main, sidebar, buttons } = props;
+  return (<div className="contacts-app">
+    <div className="panel">
+      <img alt="Contacts Logo" src={logoSvg} className="logo" />
+      <Link to="/contact/add" className="menu-button">
+        <FaPlus size={30} color={'white'} />
+      </Link>
+    </div>
+    <div className="sidebar">
+      {sidebar}
+    </div>
+    <Toolbar buttons={buttons} />
+    <div className="main">
+      {main}
+    </div>
+  </div>);
 }
+
+AppLayout.propTypes = {
+  main: PropTypes.element,
+  sidebar: PropTypes.element,
+  buttons: PropTypes.element,
+};
